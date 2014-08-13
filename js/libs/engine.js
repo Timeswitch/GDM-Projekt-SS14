8e588643f9269f709b2869593e75889ef487a370
Michael Kroll <Michael.Kroll.94@googlemail.com>
2014-08-13 23:51:20 +0200

    Änderung des Lade-Codes

--- js/libs/engine.js
+++ js/libs/engine.js
@@ -2,14 +2,21 @@
 //require();
 
 //Spiel Engine
-function Engine(){  
+function Engine(){
+    
     this.init = function(){
+        var self = this;
         
         $('body').children().fadeOut({
             duration: 'slow',
             complete: function(){
                 $('link[href="css/loading.css"]').remove();
+                self.initUI();
             }
         });
+    };
+    
+    this.initUI = function(){
+        $('body').append('Geladen, wow!');
     };
 };
