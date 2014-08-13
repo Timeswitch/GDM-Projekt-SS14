1acb24f65ca89c85ca7fc84f59f4615ecdce6da4
Michael Kroll <Michael.Kroll.94@googlemail.com>
2014-08-13 23:35:06 +0200

    jQuery + Ladebildschirm

--- js/libs/engine.js
+++ js/libs/engine.js
@@ -3,5 +3,13 @@
 
 //Spiel Engine
 function Engine(){  
-    
+    this.init = function(){
+        
+        $('body').children().fadeOut({
+            duration: 'slow',
+            complete: function(){
+                $('link[href="css/loading.css"]').remove();
+            }
+        });
+    };
 };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-require(["js/libs/engine.js"],function(){
+
+//Main Script
+
+require(["libs/external/jquery-2.1.1.min","js/libs/engine.js"],function(){ //Warum braucht man für jQuery einen anderen relativen Pfad?
     
     var game = new Engine();
+    game.init();
     
 });
 
