0963fe7c025d0fc25b907a3ba222899142d564de
Michael Kroll <Michael.Kroll.94@googlemail.com>
2014-09-06 15:00:01 +0200

    #toolbar Selektor Fix

--- js/controller/MainController.js
+++ js/controller/MainController.js
@@ -6,18 +6,18 @@ define(
         function(Controller, $, ColorGame){
             function MainController(engine, target){
                 Controller.call(this, engine, target);
-                this.cg = new ColorGame($('#canvas'),$('#toolbox'));
+                this.cg = new ColorGame($('#canvas'),$('#toolbar'));
             }
             
             MainController.prototype = Object.create(Controller.prototype);
             MainController.prototype.constructor = MainController;
             
             MainController.prototype.init = function(){
+                
                 console.log("WebApp: " + window.navigator.standalone + "\nMobile: " + window.mobile);
                 $(window).resize(function(){console.log($(window).width()/$(window).height());});
                 this.cg.$toolbar.html("<h1>hi!</h1>");
                 console.log(this.cg);
-                $('#toolbar').html('<h1>efefe</h1>');
             };
             
             return MainController;
