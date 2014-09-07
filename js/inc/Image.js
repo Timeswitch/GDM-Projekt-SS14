f80c5d38341f6155e0258e53b55ede01880ff367
Michael Kroll <Michael.Kroll.94@googlemail.com>
2014-09-08 00:00:02 +0200

    Image.each

--- js/inc/Image.js
+++ js/inc/Image.js
@@ -8,6 +8,25 @@ define(
             this.colors = Array();
         }
         
+        Image.each = function(svg,func){
+             var col = null;
+                var i = 1;
+                do{
+                    col = svg.get('col-'+i);
+                    if(typeof(col) !== 'undefined'){
+                        
+                        if(typeof(func) !== 'undefined'){
+                            func.call(col,'col-'+i,col);
+                        }
+                        
+                        
+                    }else{
+                        col = null;
+                    }
+                }
+                while(col !== null);
+        };
+        
         return Image;
     }
 );
