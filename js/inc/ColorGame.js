cb487241b1f7989b58130fb87402d8f70add7406
Michael Kroll <Michael.Kroll.94@googlemail.com>
2014-09-08 00:12:13 +0200

    Animation beim Laden (indev)

--- js/inc/ColorGame.js
+++ js/inc/ColorGame.js
@@ -1,11 +1,13 @@
 define(
         "inc/ColorGame",
-        ["lib/svg",
+        [
+         "inc/Image",
+         "lib/svg",
          "jquery",
          "lib/jquery-ui",
-         "lib/jquery.ui.touch-punch"
+         "lib/jquery.ui.touch-punch",
         ],
-        function(SVG,$){
+        function(Image,SVG,$){
             
             function ColorGame($canvas, $toolbar, engine){
                 this.$canvas = $canvas;
@@ -47,11 +49,39 @@ define(
             };
             
             ColorGame.prototype.load = function(image){
+                var self = this;
+                
                 this.image = this.engine.loadImage(image);
                 
                 this.$canvas.empty();
                 this.svg = SVG(this.$canvas.attr('id')).size('100%','100%');
-                this.svg.image('img/'+this.image.image).size('100%','100%');
+                this.svg.image('img/'+this.image.image).loaded(function(){
+                    this.size('100%','100%');
+                    self.animate();
+                });
+                
+            };
+            
+            ColorGame.prototype.initImage = function(){
+                var self = this;
+                console.log(this.svg);
+               Image.each(this.svg,function(index,element){
+                   console.log(index);
+                   var fill = {
+                        original: element.attr('fill'),
+                        current: '#ffffff'
+                    };
+
+                    self.image.colors[index] = fill;
+               });
+            };
+            
+            ColorGame.prototype.animate = function(){
+                if(this.image.colors.length === 0){
+                    this.initImage();
+                }
+                
+                
             };
             
             return ColorGame;
